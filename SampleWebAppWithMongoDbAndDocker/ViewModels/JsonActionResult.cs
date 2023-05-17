b87ae56b47ac59b88d8f8748f996697b97c5919a
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2023-05-17 21:07:07 +0500

    Bug with 204 status code as default for empty jsonActionResult parameters is fixed

--- SampleWebAppWithMongoDbAndDocker/ViewModels/JsonActionResult.cs
+++ SampleWebAppWithMongoDbAndDocker/ViewModels/JsonActionResult.cs
@@ -7,24 +7,24 @@ namespace SampleWebAppWithMongoDbAndDocker.ViewModels
 	{
 		private JsonResultObject<T> result = new JsonResultObject<T>();
 
-		public JsonActionResult(T data, int statusCode, string[] errorMessages)
+		public JsonActionResult(T data, int statusCode, string[]? errorMessages)
 		{
 			result.StatusCode = statusCode;
-			result.ErrorMessages = errorMessages;
+			result.ErrorMessages = errorMessages ?? new string[0];
 			result.DateTime = DateTime.UtcNow;
 			result.Body = data;
 		}
 
-		public JsonActionResult() : this(null!, StatusCodes.Status204NoContent, new string[0])
+		public JsonActionResult() : this(null!, StatusCodes.Status200OK, default)
 		{ }
 
-		public JsonActionResult(T data) : this(data, StatusCodes.Status200OK, new string[0])
+		public JsonActionResult(T data) : this(data, StatusCodes.Status200OK, default)
 		{ }
 
 		public JsonActionResult(string[] errorMessages) : this(null!, StatusCodes.Status400BadRequest, errorMessages)
 		{ }
 
-		public JsonActionResult(T data, int statusCode) : this(data, statusCode, new string[0])
+		public JsonActionResult(T data, int statusCode) : this(data, statusCode, default)
 		{ }
 
 		public JsonActionResult(T data, string[] errorMessages) : this(data, StatusCodes.Status400BadRequest, errorMessages)
